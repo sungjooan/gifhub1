bc7c058ceaf523b8742abee0d70533faa722379b
sungjoo <sungjooan@gmail.com>
2019-09-06 06:57:21 +0900

    trial 1

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
 print("Hello")
+print(8/3)
+print(8 / 3)
+print (8//3)
+print(2**4)
+print(16//2)
+print(16**0.5)
+print(15%4)
+string1 = "hello"
+string2 = "world"
+string3 = string1 + string2
+print(string3)
+string4 = string3*5
+print(string4)
+my_name = "helloworld"
+my_num = 123
+thisTime = 12.34
+type(my_name)
